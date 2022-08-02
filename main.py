57a70fe640c1cdc8dd206d08f6fb5036b063b14c
CDR_Clueless <joe.david64@btinternet.com>
2022-08-02 16:49:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 16:45:10 2022
+
+@author: joe
+"""
+
+from PIL import Image
+
+
+def main():
+    
+    
+if(__name__=="__main__"): main()
