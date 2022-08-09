8f1cd5c3ef42b5f99d9679b056fee21c59c8d8a0
CDR_Clueless <joe.david64@btinternet.com>
2022-08-09 13:26:47 +0100

    Variable updates
    Added a binarise threshold, probability field deviation, and minimum + maximum probabilities which can all be customised

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ Created on Tue Aug  2 16:45:10 2022
 from PIL import Image,ImageOps
 import numpy as np
 
+binarise_thresh,probfield_dev,max_black_prob,min_black_prob = 127,40,0.95,0.0
 
 def main():
     # Get the image as a grayscaled numpy array
-    imagedir = "test-blurrer.png"
+    imagedir = "test-blurrer-2.png"
     with Image.open(imagedir) as im:
         im.show()
         imarr = np.array(ImageOps.grayscale(im))
     # Binarise the existing image to 0's (white) or 1's (black)
-    imarr = binarise_image(imarr)
+    imarr = binarise_image(imarr,thresh=binarise_thresh)
     # Get a matrix of distances to black based on this
     imdist = gen_distfield(imarr)
     distance_image = Image.fromarray(np.array(imdist,dtype=np.uint8))
     distance_image.show()
     # Get a matrix of probabilities that the pixel in question will be black based on this
-    improb = gen_probfield(imdist,dev=40)
+    improb = gen_probfield(imdist,dev=probfield_dev,max_rand = max_black_prob,min_rand = min_black_prob)
     # Convert these probabilties to blacks and whites
     out = np.array(gen_binarr(improb),dtype=np.uint8)
     output_image = Image.fromarray(out)
@@ -48,20 +49,20 @@ def binarise_image(imarr,thresh=127):
     return imarr
 
 # Convert a numpy array of 1's and 0's to one of probabilities based on distance to 1's and a standard deviation
-def gen_probfield(imarr,dev=25.0,reweight = True):
+def gen_probfield(imarr,dev=25.0,max_rand = 0.95,min_rand = 0.0,reweight = False):
     prob_arr = np.zeros(imarr.shape,dtype=float)
     for i in range(len(imarr)):
         for j in range(len(imarr[i])):
-            prob_arr[i][j] = custom_sigmoid(imarr[i][j],dev)
+            prob_arr[i][j] = custom_sigmoid(imarr[i][j],dev,max_rand,min_rand)
     # If reweight is true, take the minimum and maximum probabilities, and linearly reweight them to 1 and 0
     return prob_arr
 
 # The custom sigmoid function used to get probabilities based on distance
-def custom_sigmoid(x,dev=25.0,max_rand = 0.95):
-    return min(2 - np.divide(2,1+np.exp(-1*x*np.divide(1,dev*1.5))),max_rand)
+def custom_sigmoid(x,dev=25.0,max_rand = 0.95,min_rand = 0.0):
+    return max(min(2 - np.divide(2,1+np.exp(-1*x*np.divide(1,dev*1.5))),max_rand),min_rand)
 
 # Create a matrix of distances from black pixels
-def gen_distfield(imarr,maxdist=1000,every_check=50,upperlim = np.iinfo(np.int32).max):
+def gen_distfield(imarr,maxdist=2000,every_check=50,upperlim = np.iinfo(np.int32).max):
     distfield = np.zeros(imarr.shape,dtype=int)
     cur = []
     # Set the initial values to all infinity, while marking all the starting points
